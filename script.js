526584143a157f21b2ff7966b10e64602ead90a4
noortones <noortones99@gmail.com>
2026-03-17 00:41:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,43 @@ fetch("ringtones.json")
 
 .then(data=>{
 
-let container=document.getElementById("ringtone-list")
+let all=document.getElementById("all")
+let trending=document.getElementById("trending")
 
-data.forEach(r=>{
+data.forEach((r,i)=>{
+
+let card=createCard(r)
+
+all.appendChild(card)
+
+if(i<6){
+
+trending.appendChild(createCard(r))
+
+}
+
+})
+
+document.getElementById("search").oninput=function(){
+
+let q=this.value.toLowerCase()
+
+document.querySelectorAll(".card").forEach(c=>{
+
+let t=c.querySelector("h3").innerText.toLowerCase()
+
+c.style.display=t.includes(q)?"block":"none"
+
+})
+
+}
+
+})
+
+function createCard(r){
+
+let downloads=localStorage.getItem("dl_"+r.file)||0
+let likes=localStorage.getItem("like_"+r.file)||0
 
 let card=document.createElement("div")
 
@@ -16,14 +50,85 @@ card.innerHTML=`
 
 <h3>${r.title}</h3>
 
-<audio controls src="audio/${r.file}"></audio>
+<audio src="audio/${r.file}" controls></audio>
+
+<div class="duration">⏱ ${r.duration}s</div>
+
+<div class="counter">⬇ ${downloads} downloads</div>
+
+<div class="actions">
+
+<span class="like">❤️ ${likes}</span>
+
+<span class="share">🔗</span>
+
+</div>
 
 <a class="download" href="audio/${r.file}" download>Download</a>
 
+<div class="tags">
+
+${r.tags.map(t=>`<span class="tag">${t}</span>`).join("")}
+
+</div>
+
 `
 
-container.appendChild(card)
+/* like */
+
+card.querySelector(".like").onclick=function(){
+
+likes++
+
+localStorage.setItem("like_"+r.file,likes)
+
+this.innerText="❤️ "+likes
+
+}
+
+/* share */
+
+card.querySelector(".share").onclick=function(){
+
+navigator.share({
+
+title:r.title,
+url:location.href
 
 })
 
+}
+
+/* download counter */
+
+card.querySelector(".download").onclick=function(){
+
+downloads++
+
+localStorage.setItem("dl_"+r.file,downloads)
+
+}
+
+/* tag filter */
+
+card.querySelectorAll(".tag").forEach(tag=>{
+
+tag.onclick=function(){
+
+let t=this.innerText.toLowerCase()
+
+document.querySelectorAll(".card").forEach(c=>{
+
+let tags=c.innerText.toLowerCase()
+
+c.style.display=tags.includes(t)?"block":"none"
+
 })
+
+}
+
+})
+
+return card
+
+}
