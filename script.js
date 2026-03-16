d28bfd933356a735099a80fa23d9a7866af9d1fa
noortones <noortones99@gmail.com>
2026-03-17 02:26:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,31 @@
-const ringtones = [
+fetch("./ringtones.json")
+.then(res=>res.json())
+.then(data=>{
+  let all=document.getElementById("all")
+  let trending=document.getElementById("trending")
 
-{
-title:"New Mobile Ringtone",
-file:"new ringtone 2026.mp3",
-duration:"25",
-category:"Mobile Ringtones",
-trending:true
-},
+  data.forEach(r=>{
+    let card=document.createElement("div")
+    card.className="card"
+    card.innerHTML=`
+      <h3>${r.title}</h3>
+      <audio src="./audio/${r.file}" controls></audio>
+      <div>⏱ ${r.duration}s</div>
+      <button class="download">Download</button>
+    `
 
-{
-title:"Notification Tone",
-file:"notify.mp3",
-duration:"10",
-category:"notification"
-}
+    card.querySelector(".download").onclick=function(e){
+      e.preventDefault()
+      /* ADS_PLACE_DOWNLOAD */
+      window.open("ADS_PLACE_DOWNLOAD_LINK")
+      setTimeout(()=>{window.location.href="./audio/"+r.file},3000)
+    }
 
-]
+    card.onclick=function(){
+      window.location.href="./ringtone.html?file="+r.file
+    }
 
-let all=document.getElementById("all")
-let trending=document.getElementById("trending")
-
-ringtones.forEach(r=>{
-
-let card=document.createElement("div")
-
-card.className="card"
-
-card.innerHTML=`
-
-<h3>${r.title}</h3>
-
-<audio src="audio/${r.file}" controls></audio>
-
-<div>⏱ ${r.duration}s</div>
-
-<button class="download">Download</button>
-
-`
-
-card.querySelector(".download").onclick=function(e){
-
-e.preventDefault()
-
-window.open("ADS_PLACE_DOWNLOAD_LINK")
-
-setTimeout(()=>{
-
-window.location.href="audio/"+r.file
-
-},3000)
-
-}
-
-card.onclick=function(){
-
-window.location.href="ringtone.html?file="+r.file
-
-}
-
-all.appendChild(card)
-
-if(r.trending){
-
-trending.appendChild(card.cloneNode(true))
-
-}
-
+    all.appendChild(card)
+    if(r.trending){ trending.appendChild(card.cloneNode(true)) }
+  })
 })
