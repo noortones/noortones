6f190851c395385bbda5843de6e8c659ba9dca9e
noortones <noortones99@gmail.com>
2026-03-22 13:25:09 +0530

    Update data.js

--- data.js
+++ data.js
@@ -9,4 +9,8 @@ for(let i=1;i<=100;i++){
     file:"tone"+((i%5)+1)+".mp3", // demo reuse
     category:categories[i%5]
   });
-}
+}const songs = [
+  {id:1,name:"Ringtone 1"},
+  {id:2,name:"Ringtone 2"},
+  {id:3,name:"Ringtone 3"}
+];
