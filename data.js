5ff40cb67164302c3003b4e5be6629519071a73a
noortones <noortones99@gmail.com>
2026-03-22 13:30:43 +0530

    Update data.js

--- data.js
+++ data.js
@@ -2,15 +2,11 @@ const songs = [];
 
 const categories = ["Naat","Nasheed","Trending","iPhone","Notification"];
 
-for(let i=1;i<=100;i++){
+for(let i=1;i<=20;i++){  // abhi 20 test ke liye
   songs.push({
     id:i,
     name:"Ringtone "+i,
-    file:"tone"+((i%5)+1)+".mp3", // demo reuse
+    file:"tone1.mp3",
     category:categories[i%5]
   });
-}const songs = [
-  {id:1,name:"Ringtone 1"},
-  {id:2,name:"Ringtone 2"},
-  {id:3,name:"Ringtone 3"}
-];
+}
